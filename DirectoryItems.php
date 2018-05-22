d44b789502fe5af7889429e0e6bd3edf5450bf20
WilliamDk <asabawilliamdk@yahoo.com>
2018-05-22 20:40:27 +0300

    add more info

--- DirectoryItems.php
+++ DirectoryItems.php
@@ -8,9 +8,17 @@ class DirectoryItems{
     function DirectoryItems($directory){
         $d = '';
         if (is_dir($directory)) {
-            
+            $d = opendir($directory) 
+               or die("Could not open directory");
+               while (false !== ($f = readdir($d))) {
+                   if (is_file("$directory/$f")) {
+                       $this->filearray[] = $f;
+                   }
+               }
+               closedir($d);
         } else {
-            # code...
+            // error
+            die("Must pass in a directory");
         }
         
     }
