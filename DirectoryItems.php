db3a032a8c28a41254eb23955d73dbe3b1a074ac
WilliamDk <asabawilliamdk@yahoo.com>
2018-05-22 20:11:14 +0300

    added DirectoryItem.php

---
+++ DirectoryItems.php
@@ -0,0 +1,19 @@
+<?php
+
+// Creatinf a class
+class DirectoryItems{
+    var $filearray = array();
+
+    // Creating the constructor
+    function DirectoryItems($directory){
+        $d = '';
+        if (is_dir($directory)) {
+            
+        } else {
+            # code...
+        }
+        
+    }
+}
+
+?>
